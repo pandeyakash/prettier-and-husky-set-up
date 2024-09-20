ac153ae99bd0abbcc20f0d0622cfb5b703d14e4c
pandeyakash <pandeyakash0502@gmail.com>
2024-09-20 10:21:33 +0530

    Completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello from react project</h1>
+      <h2>Setting up Prettier with husky</h2>
+    </>
+  );
+}
+
+export default App;
